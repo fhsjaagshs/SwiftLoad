96418a0cdbb8160a6b9915923487eb0946688ef5
Nathaniel Symer <nate@natesymer.com>
2013-06-29 00:09:31 -0400

    More settings cleanup

--- downloader/SettingsView.m
+++ downloader/SettingsView.m
@@ -22,17 +22,13 @@
     topItem.leftBarButtonItem = [[[UIBarButtonItem alloc]initWithTitle:@"Close" style:UIBarButtonItemStyleBordered target:self action:@selector(close)]autorelease];
     [bar pushNavigationItem:topItem animated:NO];
     [self.view addSubview:bar];
-    [self.view bringSubviewToFront:bar];
     [bar release];
     [topItem release];
     
-    CGRect linkButtonFrame = CGRectMake(100, sanitizeMesurement(189), 120, 37);
-    CGRect bmbFrame = CGRectMake(75, sanitizeMesurement(265), 170, 37);
+    BOOL iPad = (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad);
     
-    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
-        linkButtonFrame = CGRectMake(313, 396, 143, 37);
-        bmbFrame = CGRectMake(302, 483, 164, 37);
-    }
+    CGRect linkButtonFrame = iPad?CGRectMake(313, 396, 143, 37):CGRectMake(100, sanitizeMesurement(189), 120, 37);
+    CGRect bmbFrame = iPad?CGRectMake(302, 483, 164, 37):CGRectMake(75, sanitizeMesurement(265), 170, 37);
     
     UIImage *buttonImage = [[UIImage imageNamed:@"button_icon"]resizableImageWithCapInsets:UIEdgeInsetsMake(4, 4, 4, 4)];
     
@@ -41,10 +37,9 @@
     _linkButton.titleLabel.font = [UIFont boldSystemFontOfSize:17];
     [_linkButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
     [_linkButton setBackgroundImage:buttonImage forState:UIControlStateNormal];
-    self.linkButton.autoresizingMask = UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin;
-    [self.linkButton addTarget:self action:@selector(linkOrUnlink) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:self.linkButton];
-    [self.view bringSubviewToFront:self.linkButton];
+    _linkButton.autoresizingMask = UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin;
+    [_linkButton addTarget:self action:@selector(linkOrUnlink) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:_linkButton];
     
     UIButton *bookmarkletButton = [UIButton buttonWithType:UIButtonTypeCustom];
     bookmarkletButton.frame = bmbFrame;
@@ -55,7 +50,6 @@
     [bookmarkletButton addTarget:self action:@selector(showBookmarkletInstallationAV) forControlEvents:UIControlEventTouchUpInside];
     [bookmarkletButton setTitle:@"Install Bookmarklet" forState:UIControlStateNormal];
     [self.view addSubview:bookmarkletButton];
-    [self.view bringSubviewToFront:bookmarkletButton];
     
     [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(dropboxAuthenticationSucceeded) name:@"db_auth_success" object:nil];
     [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(dropboxAuthenticationFailed) name:@"db_auth_failure" object:nil];
@@ -66,11 +60,11 @@
 }
 
 - (void)showBookmarkletInstallationAV {
-    TransparentAlert *cav = [[TransparentAlert alloc]initWithTitle:@"Install Bookmarklet?" message:@"The bookmarklet allows you to download the open webpage or file from Safari.\n\nClicking \"sure\" will replace your clipboard's content." completionBlock:^(NSUInteger buttonIndex, UIAlertView *alertView) {
+    TransparentAlert *cav = [[TransparentAlert alloc]initWithTitle:@"Install Bookmarklet?" message:@"This bookmarklet allows you to download any file open in Safari. Clicking \"Sure!\" will overwrite your clipboard's content." completionBlock:^(NSUInteger buttonIndex, UIAlertView *alertView) {
         
         if (buttonIndex == 1) {
             [[UIPasteboard generalPasteboard]setString:@"JavaScript:string=document.URL;anotherString=string.replace('http://','swiftload://');window.open(anotherString);"];
-            TransparentAlert *av = [[TransparentAlert alloc]initWithTitle:@"Bookmarklet Copied!" message:@"Open Safari and create a bookmark, naming it \"SwiftLoad Download\". Enter editing mode in the bookmarks menu and tap the newly created bookmark. Paste the contents of the clipboard to the URL field of the newly created bookmark, replacing whatever is there. Tap done and exit editing mode. To download files, just navigate to a page and tap the bookmark." delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+            TransparentAlert *av = [[TransparentAlert alloc]initWithTitle:@"Bookmarklet Copied!" message:@"Open Safari and create a bookmark, naming it \"SwiftLoad Download\". Enter editing mode in the bookmarks menu and tap the newly created bookmark. Paste the contents of the clipboard to the URL field of the newly created bookmark, replacing whatever is already there. Tap done then exit editing mode. To download files, just navigate to a page and tap the bookmark." delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
             [av show];
             [av release];
         }
@@ -81,8 +75,7 @@
 }
 
 - (void)sessionDidReceiveAuthorizationFailure:(DBSession *)session userId:(NSString *)userId {
-    TransparentAlert *asdf = [[[TransparentAlert alloc]initWithTitle:@"Login Failed" message:@"There was an error in trying to log into Dropbox. Please try again later." delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil]autorelease];
-    [asdf show];
+    [[[[TransparentAlert alloc]initWithTitle:@"Login Failed" message:@"There was an error in trying to log into Dropbox. Please try again later." delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil]autorelease]show];
 }
 
 - (void)linkOrUnlink {
@@ -96,11 +89,7 @@
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
-    if ([[DBSession sharedSession]isLinked]) {
-        [self.linkButton setTitle:@"Unlink Dropbox" forState:UIControlStateNormal];
-    } else {
-        [self.linkButton setTitle:@"Link Dropbox" forState:UIControlStateNormal];
-    }
+    [_linkButton setTitle:[[DBSession sharedSession]isLinked]?@"Unlink Dropbox":@"Link Dropbox" forState:UIControlStateNormal];
 }
 
 - (void)dropboxAuthenticationFailed {
