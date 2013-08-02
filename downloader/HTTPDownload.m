afe4178827e257cca138136d78382a812c839538
Nathaniel Symer <nate@natesymer.com>
2013-08-02 10:05:34 -0400

    Remove writability handler from HTTPDownload

--- downloader/HTTPDownload.m
+++ downloader/HTTPDownload.m
@@ -14,10 +14,6 @@
 @property (nonatomic, assign) float downloadedBytes;
 @property (nonatomic, assign) float fileSize;
 @property (nonatomic, strong) NSFileHandle *handle;
-@property (nonatomic, strong) NSMutableData *buffer;
-
-@property (nonatomic, assign) BOOL isAppending;
-@property (nonatomic, assign) float writtenBytes;
 
 @end
 
@@ -32,14 +28,12 @@
     if (self) {
         self.url = aUrl;
         self.name = [_url.absoluteString.lastPathComponent percentSanitize];
-        self.buffer = [NSMutableData data];
     }
     return self;
 }
 
 - (void)stop {
     [_connection cancel];
-    [_buffer setLength:0];
     [_handle closeFile];
     self.downloadedBytes = 0;
     self.fileSize = 0;
@@ -48,8 +42,6 @@
 
 - (void)start {
     [super start];
-    
-    [_buffer setLength:0];
     
     NSMutableURLRequest *theRequest = [NSMutableURLRequest requestWithURL:_url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:30.0];
     [theRequest setHTTPMethod:@"GET"];
@@ -72,53 +64,21 @@
     
     self.temporaryPath = getNonConflictingFilePathForPath([NSTemporaryDirectory() stringByAppendingPathComponent:[self.name percentSanitize]]);
     self.fileSize = [response expectedContentLength];
-    NSLog(@"FileSize: %f",self.fileSize);
     [[NSFileManager defaultManager]createFileAtPath:self.temporaryPath contents:nil attributes:nil];
     self.handle = [NSFileHandle fileHandleForWritingAtPath:self.temporaryPath];
-    
-    __weak HTTPDownload *weakself = self;
-    
-    if (_fileSize != -1) {
-        [_handle setWriteabilityHandler:^(NSFileHandle *handle) {
-            if (!weakself.isAppending && weakself.buffer.length > 0) {
-                NSData *data = [weakself.buffer copy];
-                [weakself.buffer setLength:0];
-                [handle seekToEndOfFile];
-                [handle writeData:data];
-                [handle synchronizeFile];
-                weakself.writtenBytes += data.length;
-                
-                if (weakself.writtenBytes == weakself.fileSize) {
-                    [handle closeFile];
-                    dispatch_sync(dispatch_get_main_queue(), ^{
-                        @autoreleasepool {
-                            [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
-                            [weakself showSuccess];
-                        }
-                    });
-                }
-            }
-        }];
-    }
+    [_handle seekToEndOfFile];
 }
 
 - (void)connection:(NSURLConnection *)theConnection didReceiveData:(NSData *)receivedData {
     self.downloadedBytes += receivedData.length;
-    
-    if (_fileSize == -1) {
-        [_handle writeData:receivedData];
-    } else {
-        self.isAppending = YES;
-        [_buffer appendData:receivedData];
-        self.isAppending = NO;
-    }
-
+    [_handle seekToEndOfFile];
+    [_handle writeData:receivedData];
+    [_handle synchronizeFile];
     [self.delegate setProgress:((_fileSize == -1)?1:((float)_downloadedBytes/(float)_fileSize))];
 }
 
 - (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
     [_connection cancel];
-    [_buffer setLength:0];
     [_handle closeFile];
     self.downloadedBytes = 0;
     self.fileSize = 0;
@@ -128,12 +88,11 @@
 - (void)connectionDidFinishLoading:(NSURLConnection *)theConnection {
     [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
 
-    if (_fileSize == -1) {
-        [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
-        [self showSuccess];
-    }
+    [_handle closeFile];
     
-    if (_downloadedBytes == 0) {
+    if (_downloadedBytes > 0) {
+        [self showSuccess];
+    } else {
         [self showFailure];
     }
 }
