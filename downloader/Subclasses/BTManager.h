48e788b4767c83e450dafc150c6eeefc97e466ad
Nathaniel Symer <nate@natesymer.com>
2013-09-30 19:33:39 -0400

    Move to hackintosh

---
+++ downloader/Subclasses/BTManager.h
@@ -0,0 +1,13 @@
+//
+//  BTManager.h
+//  Swift
+//
+//  Created by Nathaniel Symer on 9/30/13.
+//  Copyright (c) 2013 Nathaniel Symer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BTManager : NSObject
+
+@end
---
+++ downloader/Subclasses/BTManager.m
@@ -0,0 +1,15 @@
+//
+//  BTManager.m
+//  Swift
+//
+//  Created by Nathaniel Symer on 9/30/13.
+//  Copyright (c) 2013 Nathaniel Symer. All rights reserved.
+//
+
+#import "BTManager.h"
+
+@implementation BTManager
+
+
+
+@end
